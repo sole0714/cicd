919a9ee6d8a21695d6a1ad0ae0fbcabf86e7e9d0
sole.lhb <sole.lhb@gmail.com>
2026-04-06 11:46:32 +0900

    [feat] test push

--- src/main/java/com/example/jen/CicdDto.java
+++ src/main/java/com/example/jen/CicdDto.java
@@ -1,4 +1,14 @@
 package com.example.jen;
 
+import lombok.AllArgsConstructor;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class CicdDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
 }
